4c4ced18631156fcce7cf29739ec47f48edad961
GlennRuark <gr20ou@brocku.ca>
2024-03-22 14:04:10 -0400

    Update week9

---
+++ week9/script.js
@@ -0,0 +1,66 @@
+import * as THREE from "three"
+import * as dat from "lil-gui"
+import { OrbitControls } from "OrbitControls"
+
+/**********
+** SETUP **
+***********/
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+/**********
+** SCENE **
+***********/
+// Canvas
+const canvas = document.querySelector('.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('white')
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes,aspectRatio,
+    0.1,
+    100
+)
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Orbit Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/*******************
+** ANIMATION LOOP **
+********************/
+const clock = new THREE.Clock()
+
+// Animate
+const animation = () =>
+{
+    // Return elapsedTime
+    const elapsedTime = clock.getElapsedTime()
+
+    // Orbit Controls
+    controls.update()
+
+    // Renderer
+    renderer.render(scene, camera)
+
+    // Request next frame
+    window.requestAnimationFrame(animation)
+}
+
+animation()
